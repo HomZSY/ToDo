904e641f649f9b9aa170813dae5b199d33508a80
HomZSY <775384893@qq.com>
2016-06-16 22:08:29 +0800

    change

--- Public/assets/pages/scripts/todo-index.js
+++ Public/assets/pages/scripts/todo-index.js
@@ -1,12 +1,7 @@
-$(function(){
-	var speen = 150;
- 	outPlay = function(){
-		setInter = setInterval("out_fun()",speen); //setInterval间隔指定的毫秒数不停地执行指定的代码
-				  //设置时间控制器					   //out_fun()方法进行轮播
-	}
-	outPlay(); //一定需要这一句话，这是方法的执行
-	out_fun = function(){
+var Index = function () {
+	title = function () {
 		$(".title_div .titlel").fadeToggle("slow");
-		$(".title_div .titler").fadeToggle('slow');
-	}
-})
+		$(".title_div .titler").fadeToggle("slow");
+	};
+	setInterval("title()",150);
+}();
