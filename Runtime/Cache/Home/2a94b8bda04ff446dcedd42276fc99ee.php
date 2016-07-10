8d8ba83a0ecf862c91e3098e6cd303a07e959b10
HomZSY <775384893@qq.com>
2016-07-10 23:08:34 +0800

    change

--- Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -2,25 +2,25 @@
 <html lang="en" class="no-js">
 <head>
     <meta charset="utf-8"/>
-    <link rel="shortcut icon" href="/todo-11-18/Public/assets/pages/img/index/avatar.jpg"/>
+    <link rel="shortcut icon" href="/todo1/todo2/Public/assets/pages/img/index/avatar.jpg"/>
     <title>土豆鸡快</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
     <meta name="author" content="zsy"/>
 
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
-        <link rel="stylesheet" href="/todo-11-18/Public/assets/global/plugins/bootstrap/css/bootstrap.min.css"/>
-        <link href="/todo-11-18/Public/assets/global/styles/reset.css" rel="stylesheet" type="text/css"/>
-        <link href="/todo-11-18/Public/assets/global/styles/style.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="/todo1/todo2/Public/assets/global/plugins/bootstrap/css/bootstrap.min.css"/>
+    
+        <link href="/todo1/todo2/Public/assets/global/styles/style.css" rel="stylesheet" type="text/css"/>
     <!-- END GLOBAL MANDATORY STYLES -->
 
     <!-- BEGIN PAGE LEVEL PLUGIN STYLES -->
-        <link href="/todo-11-18/Public/assets/global/plugins/icon/style.css" rel="stylesheet" type="text/css"/>
+        <link href="/todo1/todo2/Public/assets/global/plugins/icon/style.css" rel="stylesheet" type="text/css"/>
     <!-- END PAGE LEVEL PLUGIN STYLES -->
 
     <!-- BEGIN PAGE STYLES -->
         
-	<link rel="stylesheet" type="text/css" href="/todo-11-18/Public/assets/pages/styles/todo-index.css">
+	<link rel="stylesheet" type="text/css" href="/todo1/todo2/Public/assets/pages/styles/todo-index.css">
 
     <!-- END PAGE STYLES -->
 </head>
@@ -83,20 +83,14 @@
     <section class="page-body">
         
 	<div class="title_div">
-		<div class="titlel"> </div>
+		<div class="titlel"></div>
 		<div class="titler"></div>
 	</div>
 	<a href="<?php echo U('List/todo_list');?>">
-		<div class="scroll_div">
-			<ul class="pic">
-				<li class="l"></li>
-				<li class="lc"></li>
-				<li class="rc"></li>
-				<li class="r"></li>
-			</ul>
+		<div class="kick">
 		</div>
 	</a>
-    <?php if(empty($username)): else: ?><h3 style="text-align: center;">Hello,<?php echo ($username); ?>!</h3><?php endif; ?>
+    <?php if(empty($username)): else: ?><h3 style="text-align: center;" class="hello">Hello,<?php echo ($username); ?>!</h3><?php endif; ?>
 <!-- page footer -->
 	<a href="<?php echo U('User/login');?>">
 		<div class="logo"></div>
@@ -106,33 +100,33 @@
 <!-- END PAGE-BODY -->
 
 <!-- BEGIN CORE PLUGINS -->
-    <script src="/todo-11-18/Public/assets/global/plugins/jquery-2.1.1.js" type="text/javascript"></script>
-    <script src="/todo-11-18/Public/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="/todo-11-18/Public/assets/global/plugins/modernizr.js" type="text/javascript"></script>
-    <script src="/todo-11-18/Public/assets/global/plugins/main.js" type="text/javascript"></script>
+    <script src="/todo1/todo2/Public/assets/global/plugins/jquery-2.1.1.js" type="text/javascript"></script>
+    <script src="/todo1/todo2/Public/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="/todo1/todo2/Public/assets/global/plugins/modernizr.js" type="text/javascript"></script>
+    <script src="/todo1/todo2/Public/assets/global/plugins/main.js" type="text/javascript"></script>
 <!-- END CORE PLUGINS -->
 
 <!-- BEGIN CORE GLOBAL -->
-    <script src="/todo-11-18/Public/assets/global/scripts/public.js" type="text/javascript"></script>
+    <script src="/todo1/todo2/Public/assets/global/scripts/public.js" type="text/javascript"></script>
 <!-- END CORE GLOBAL -->
 
 <script type="text/javascript">
     /* GLOBAL URL */
-    var _ROOT_ = '/todo-11-18',
-            _PUBLIC_ = '/todo-11-18/Public',
-            _INDEX_ = '/todo-11-18/index.php',
-            _ACTION_ = '/todo-11-18/index.php/Home/Index/index',
-            _MODULE_ = '/todo-11-18/index.php/Home',
-            _CONTROLLER_ = '/todo-11-18/index.php/Home/Index';
-    window._ROOT_='/todo-11-18';
-    window._APP_='/todo-11-18/index.php';
+    var _ROOT_ = '/todo1/todo2',
+            _PUBLIC_ = '/todo1/todo2/Public',
+            _INDEX_ = '/todo1/todo2/index.php',
+            _ACTION_ = '/todo1/todo2/index.php/Home/Index/index',
+            _MODULE_ = '/todo1/todo2/index.php/Home',
+            _CONTROLLER_ = '/todo1/todo2/index.php/Home/Index';
+    window._ROOT_='/todo1/todo2';
+    window._APP_='/todo1/todo2/index.php';
     window._ACTION_='<?php echo U("");?>';
-    window._SELF_='<?php echo urldecode("/todo-11-18/index.php?m=Home&c=Index&a=index");?>';
+    window._SELF_='<?php echo urldecode("/todo1/todo2/index.php?m=Home&c=Index&a=index");?>';
 </script>
 
 <!-- BEGIN PAGE LEVEL SCRIPTS -->
     
-	<script src="/todo-11-18/Public/assets/pages/scripts/todo-index.js" type="text/javascript"></script>
+	<script src="/todo1/todo2/Public/assets/pages/scripts/todo-index.js" type="text/javascript"></script>
 
 <!-- END PAGE LEVEL SCRIPTS -->
 
